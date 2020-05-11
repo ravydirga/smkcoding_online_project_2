e49ddd8885fc3f9672ed9757b7a4232505e0ddf7
Ravy Dirga <ravydirga27@gmail.com>
2020-05-11 12:11:16 +0700

    project init

--- ProjectTwo/app/src/main/java/com/smkcoding/projecttwo/GithubUser.kt
+++
@@ -1,6 +0,0 @@
-package com.smkcoding.projecttwo
-
-
-import com.google.gson.annotations.SerializedName
-
-class GithubUser : ArrayList<GithubUserItem>()
--- ProjectTwo/app/src/main/java/com/smkcoding/projecttwo/util/ToastUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.smkcoding.projecttwo.util
-
-import android.widget.Toast
-import android.content.Context
-
-
-fun tampilToast(context: Context, message:String) {
-    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-}
