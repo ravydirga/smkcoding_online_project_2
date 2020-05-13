63926c6b6192754450813712a2c64298b8c51274
Ravy Dirga <ravydirga27@gmail.com>
2020-05-13 08:55:29 +0700

    project init

---
+++ MyInfoCovid/app/src/main/java/com/smkcoding/myinfocovid/ProfilFragment.kt
@@ -0,0 +1,32 @@
+package com.smkcoding.myinfocovid
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+
+
+class ProfilFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
